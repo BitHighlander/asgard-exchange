ec79f243d0070ddd5e9f1e8123dcb9fbe38c9105
highlander <bithighlander@gmail.com>
2021-03-02 21:18:45 -0700

    begin deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
-// this is overwritten on build
-// in scripts/setenv
 
 export const environment = {
-  production: true,
-  network: 'chaosnet',
-  etherscanKey: process.env.ETHERSCAN_KEY,
-  infuraProjectId: process.env.INFURA_PROJET_ID
+   production: true,
+   network: 'chaosnet',
+   etherscanKey: '',
+   infuraProjectId: '',
 };
--- src/environments/environment.testnet.ts
+++ src/environments/environment.testnet.ts
@@ -1,9 +1,7 @@
-// this is overwritten on build
-// in scripts/setenv
 
 export const environment = {
-  production: true,
-  network: 'testnet',
-  etherscanKey: process.env.ETHERSCAN_KEY,
-  infuraProjectId: process.env.INFURA_PROJET_ID
+   production: true,
+   network: 'testnet',
+   etherscanKey: '',
+   infuraProjectId: '',
 };
