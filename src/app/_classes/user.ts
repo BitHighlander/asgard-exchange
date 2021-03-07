cc2431c207dce71cd738648967673740e5a60498
highlander <bithighlander@gmail.com>
2021-03-06 22:59:04 -0700

    begin pioneer sdk

--- src/app/_classes/user.ts
+++ src/app/_classes/user.ts
@@ -1,6 +1,6 @@
 
 
-export type WalletType = 'keystore' | 'walletconnect' | 'ledger';
+export type WalletType = 'keystore' | 'walletconnect' | 'ledger' | 'pioneer';
 import { Client as BinanceClient } from '@xchainjs/xchain-binance';
 import { Client as BitcoinClient } from '@xchainjs/xchain-bitcoin';
 import { Client as ThorchainClient } from '@xchainjs/xchain-thorchain';
@@ -21,16 +21,19 @@ export class User {
   clients?: AvailableClients;
 
   // for Ledger
+  pioneer?: any;
   ledger?: any;
   hdPath?: number [];
   balances: Balances;
   // walletConnector?: FixmeType;
 
-  constructor(user: {type: WalletType, wallet: string, keystore?: any, ledger?: any, hdPath?: number[], clients?: AvailableClients}) {
+  // tslint:disable-next-line:max-line-length
+  constructor(user: {type: WalletType, wallet: string, keystore?: any, ledger?: any, pioneer?: any, hdPath?: number[], clients?: AvailableClients}) {
     this.type = user.type;
     this.wallet = user.wallet;
     this.keystore = user.keystore ?? null;
     this.ledger = user.ledger ?? null;
+    this.pioneer = user.pioneer ?? null;
     this.hdPath = user.hdPath ?? null;
     this.clients = user.clients;
   }
